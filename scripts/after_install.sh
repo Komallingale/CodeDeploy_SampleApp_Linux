dbe94c059938810daeee6920c3fd232ef10b80bb
Komallingale <125949997+Komallingale@users.noreply.github.com>
2023-09-27 16:55:10 +0530

    Delete scripts directory

--- scripts/after_install.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-if which yum &>/dev/null; then
-  echo "Something to do AfterInstall on yum systems"
-elif which apt-get &>/dev/null; then
-  echo "Something to do AfterInstall on apt systems"
-fi
--- scripts/copy_files.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-echo "Copying files..."
-
-cp -rf ./deployment-root/${DEPLOYMENT_GROUP_ID}/${DEPLOYMENT_ID}/deployment-archive/myweb/* /var/www/html
-
-echo "Copying files... Done!"
